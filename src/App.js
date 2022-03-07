0587f8271bd45e9760452e581216b1de2a2d2c5c
GSdaBlessedFist <gsdablessedfist@gmail.com>
2022-03-07 17:51:03 -0600

    1)homepageMenu created...2)svgr created for ti...NEXT::  Unable to manipulate homepageGrid

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import "./app.scss";
+import HomePageMenu from "./components/HomePageMenu";
 
 function App() {
-  return (<>
-    <h1>This is the home page</h1>
-  </>);
+  return (
+    <div className={"homePageGrid"}>
+      <div className={"homePageGrid-space"}>space</div>
+      <div className={"homePageGrid-menu"}>svg     </div>
+      <div className={"homePageGrid-space"}>space</div>
+
+    </div>
+  );
 }
 
 export default App;
