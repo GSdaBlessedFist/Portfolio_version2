6a76eccb8c17e85e4eef85d72020bc4763ed1074
GSdaBlessedFist <gsdablessedfist@gmail.com>
2022-03-14 14:05:33 -0500

    1)started Projects page...2)projects component and fake project list started

--- src/routes/ProjectsPage.js
+++ src/routes/ProjectsPage.js
@@ -1,9 +1,32 @@
 import styles from "./projectsPage.scss";
 
+const pieces = [
+	{
+		name: "piece 1",
+		goal: "This is a test entry"
+	}
+]
 
 export default function ProjectsPage(){
 	
 	return (<>
-		<h1 className="styles.projectsPage">Check out my projects</h1>
+		<div className="projectsPageGrid">
+			<div className="projectsPageGrid--title">Projects</div>
+			{pieces.map((piece)=>{
+				return (<>
+					<div className="projectsComponent">
+						<div className="projectsComponent--descriptionSection">
+							<div className="projectsComponent--description_goal">{piece.goal}</div>	
+						</div>
+						<div className="projectsComponent--workSection">{piece.name}</div>
+					</div>
+				</>)
+			})}
+		</div>
+			
 	</>)
+}
+function ProjectsComponent({piece}){
+	return <div>HERE</div>;
+	
 }
