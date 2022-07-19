e90aa49a5ce8162b202e312169bfca863f7adcce
GSdaBlessedFist <gsdablessedfist@gmail.com>
2022-07-19 14:52:04 -0500

    1)continued work on mediaqueries on Project page...NEXT:: Aboutme page queries

--- src/projectInfo.js
+++ src/projectInfo.js
@@ -1,6 +1,16 @@
 
 const projectInfo = [
 	{
+		id: "react-movie-search",
+		name: "React-movie-search",
+		goal: "This app pulls from The Movie Database(TMDB) Api to find general info on quite a few movies.",
+		description: "This SPA pulls data from the TMDB Api using a simple fetch call.",
+		techStack: "React, SASS, TMDB Api, and Vercel",
+		screenShot: "images/movie-search-screenshot.png",
+		url: "https://tmdb-movie-search-cxueep507-gsdablessedfist.vercel.app/",
+		git: "https://github.com/GSdaBlessedFist/TMDB-Movie-Search"
+	},
+	{
 		id: "bonsai-haiku",
 		name: "a-bonsai-haiku",
 		goal: "I wanted to create a minimalist yet interactive interface to present a haiku I found.",
@@ -21,20 +31,10 @@ const projectInfo = [
 		git: "https://codepen.io/GuyShifty/pen/xxYwOwK/?layout=left"
 	},
 	{
-		id: "react-movie-search",
-		name: "React-movie-search",
-		goal: "This app pulls from The Movie Database(TMDB) Api to find general info on quite a few movies.",
-		description: "This SPA pulls data from the TMDB Api using a simple fetch call.",
-		techStack: "React, SASS, TMDB Api, and Vercel",
-		screenShot: "images/movie-search-screenshot.png",
-		url: "https://tmdb-movie-search-cxueep507-gsdablessedfist.vercel.app/",
-		git: "https://github.com/GSdaBlessedFist/TMDB-Movie-Search"
-	},
-	{
 		id: "simple-cosmic-blog",
 		name: "simple-cosmic-blog",
 		goal: "This project was simply about testing out the Cosmic CMS.",
-		description: "In addition to trying out a new CMS, I was also experimenting with a blog layout using sliding folders to hide/reveal info",
+		description: "In addition to trying out a new CMS, I was also trying out a blog layout using sliding folders to hide/reveal info",
 		techStack: "NodeJS, Express, Handlebars, SASS, Cosmic CMS, NodeMailer,and Heroku",
 		screenShot: "images/simple-cosmic-blog-screenshot.png",
 		url: "https://simple-cosmic-blog.herokuapp.com/",
