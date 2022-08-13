886177252af6aa7502b987ed96bd14a867efd14b
GSdaBlessedFist <gsdablessedfist@gmail.com>
2022-08-13 16:18:38 -0500

    1)installing new GA4 tag

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, {useEffect } from 'react';
 import {
   BrowserRouter,
   Routes,
@@ -9,10 +10,29 @@ import HomePage from "./routes/HomePage";
 import ProjectsPage from "./routes/ProjectsPage";
 import TheDojoPage from "./routes/TheDojoPage";
 import AboutMePage from "./routes/AboutMePage";
+import ReactGA from 'react-ga4';
+
+ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_MEASUREMENT_ID);
+ReactGA.send("pageview");
 
 function App() {
 
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
+        <Helmet>
+          <!-- Google tag (gtag.js) -->
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-C5LPNG85N0"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-C5LPNG85N0');
+          </script>
+        </Helmet>
         <BrowserRouter>
             <PrimeGrid>    
               <Routes>
